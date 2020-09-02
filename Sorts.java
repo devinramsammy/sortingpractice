cf8ca8c0c0dcc06bedff44451704693acef46194
Devin Ramsammy <devinram01@gmail.com>
2020-09-01 21:46:58 -0400

    added updating testing for merge sort

--- Sorts.java
+++ Sorts.java
@@ -59,9 +59,26 @@ public class Sorts {
 
 
     public static void main(String[] args) {
-        int[] edgeCase = {0,0,0,0,2,0,0,0,0};
+        int[] edgeCase = {0};
+        int[] edgeCase1 = {};
+        int[] edgeCase2 = {-23,-5,-6,-7,-231,543,0};
+        int[] edgeCase3 = {-22,-5,-5,-5,-5,5,0};
+
+        System.out.println("----------Merge Sort----------");
+        System.out.println("Testing Edge Cases");
+        System.out.println("\nEdge Case 1: " + Arrays.toString(edgeCase));
         MergeSort(edgeCase);
-         System.out.println(Arrays.toString(edgeCase));
+        System.out.println("Edge Case 1 Result: " + Arrays.toString(edgeCase));
+        System.out.println("\nEdge Case 2: " + Arrays.toString(edgeCase1));
+        MergeSort(edgeCase1);
+        System.out.println("Edge Case 2 Result: " + Arrays.toString(edgeCase1));
+        System.out.println("\nEdge Case 3: " + Arrays.toString(edgeCase2));
+        MergeSort(edgeCase2);
+        System.out.println("Edge Case 3 Result: " + Arrays.toString(edgeCase2));
+        System.out.println("\nEdge Case 4: " + Arrays.toString(edgeCase3));
+        MergeSort(edgeCase3);
+        System.out.println("Edge Case 4 Result: " + Arrays.toString(edgeCase3));
+
 }
 
 }
